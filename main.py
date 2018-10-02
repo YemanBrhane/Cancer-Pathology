0103d6c05d17fb963eb99624746e59265b74f418
YemanBrhane <yemanbrhane1989@gmail.com>
2018-10-02 13:43:29 +0200

    added  main file

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  2 13:42:39 2018
+
+@author: yb
+"""
+
+# tensorflow
+import tensorflow
+print('tensorflow: %s' % tensorflow.__version__)
+# keras
+import keras
+print('keras: %s' % keras.__version__)
